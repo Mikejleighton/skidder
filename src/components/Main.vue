6ae5413fa69cf37c22ca4de7032a7aaf884661da
Michael Leighton <Mikejleighton@gmail.com>
2020-02-12 22:53:07 -0500

    Initial checking

---
+++ src/components/Main.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar app color="black" dark>
+      <v-toolbar-title>LumberJack</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content >
+        <log-viewer></log-viewer>
+    </v-content>
+    <v-footer color="black" app>
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import LogViewer from './LogViewer';
+export default {
+  props: {
+    source: String
+  },
+
+  components: {
+    LogViewer,
+  },
+
+  data: () => ({
+    drawer: null
+  })
+}
+</script>
